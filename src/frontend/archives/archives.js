7756aec41e0c409adef1ab22254a7e53546651f4
Phanh <anh.pham.210191@student.fulbright.edu.vn>
2024-04-22 23:24:28 +0700

    intial update page archives

--- src/frontend/archives/archives.js
+++ src/frontend/archives/archives.js
@@ -3,7 +3,8 @@ import React from 'react';
 function Archives() {
   return (
     <div>
-      <p>This is the Archives</p>
+      <h1>10 projects</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Sed eget sapien vel ligula laoreet tristique. Sed consequat, mauris quis tristique condimentum,</p>
     </div>
   );
 }
--- src/frontend/homepage/homepage.js
+++ src/frontend/homepage/homepage.js
@@ -3,7 +3,8 @@ import React from 'react';
 function HomePage() {
   return (
     <div>
-      <h2>This is the Home Page</h2>
+      <h2>10 projects</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Sed eget sapien vel ligula laoreet tristique. Sed consequat, mauris quis tristique condimentum,</p>
     </div>
   );
 }
