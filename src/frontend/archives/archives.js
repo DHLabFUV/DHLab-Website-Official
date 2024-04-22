0ebf3fc8bf14b3d0b6f79a165be3eef39528efdb
Phanh <anh.pham.210191@student.fulbright.edu.vn>
2024-04-22 23:58:26 +0700

    random background

--- src/frontend/archives/archives.js
+++ src/frontend/archives/archives.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import RandomBackground from './randombg';
 
 function Archives() {
   return (
     <div>
+      <RandomBackground/>
       <h1>10 projects</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Sed eget sapien vel ligula laoreet tristique. Sed consequat, mauris quis tristique condimentum,</p>
     </div>
