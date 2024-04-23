02f80cead64af211d789d61ab17b1ecfc723693c
Phanh <anh.pham.210191@student.fulbright.edu.vn>
2024-04-24 00:29:40 +0700

    toggle list in archives page

---
+++ src/frontend/archives/togglelist.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './togglelist.css';
+
+const ToggleList = ({ items }) => {
+  const [openItem, setOpenItem] = useState(null);
+
+  // Function to toggle the open state of an item
+  const toggleItem = (itemId) => {
+    if (openItem === itemId) {
+      // If clicked item is already open, close it
+      setOpenItem(null);
+    } else {
+      // Otherwise, open the clicked item
+      setOpenItem(itemId);
+    }
+  };
+
+  return (
+    <div className="toggle-list">
+      <ul>
+        {items.map((item) => (
+          <li
+            key={item.id}
+            onClick={() => toggleItem(item.id)}
+            className={openItem === item.id ? 'active' : ''}
+          >
+            {item.title}
+            {openItem === item.id && <p className="additional-content">
+                <p>{item.content}</p>
+                <img src={item.imageUrl} alt="Additional" className='image-item' />
+            </p>}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ToggleList;
