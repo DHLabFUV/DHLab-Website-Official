cc4a0687119f77c732f54f0b08404ae3b2261c3e
Phanh <anh.pham.210191@student.fulbright.edu.vn>
2024-04-28 21:39:08 +0700

    cd

--- src/server.js
+++ src/server.js
@@ -18,6 +18,34 @@ const emailSchema = new mongoose.Schema({
   },
 });
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://pdpawork:OppAKGAj12Vl6E7m@dhweb.ecqj88o.mongodb.net/?retryWrites=true&w=majority&appName=DHWeb";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 // Create a model for emails
 const Email = mongoose.model('Email', emailSchema);
 
