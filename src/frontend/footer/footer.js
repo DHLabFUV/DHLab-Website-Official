ac605d374b24bded7b5f209e41ef701d1f1d7fa8
Phuong Anh <119726597+PhuongAnh2212@users.noreply.github.com>
2024-05-02 23:55:13 +0700

    animate footnote

--- src/frontend/footer/footer.js
+++ src/frontend/footer/footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className='grid-container'>
             <div className='formlink'>
                 {/* Use <a> tag for external link */}
-                <span href='https://www.facebook.com/'cclassName='link'>Say Hello</span>               
+                <a href='https://www.facebook.com/'cclassName='link'>Say Hello</a>               
             </div>
       </div>
   );
