f47db85767f0c13a40d2ab50196c3cf9479f2906
Phuong Anh <119726597+PhuongAnh2212@users.noreply.github.com>
2024-05-02 23:46:15 +0700

    Update footer

--- src/frontend/footer/footer.js
+++ src/frontend/footer/footer.js
@@ -4,17 +4,12 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='background'>
         <div className='grid-container'>
             <div className='formlink'>
                 {/* Use <a> tag for external link */}
-                <a href='https://www.facebook.com/'>
-                    <span className='font-sec'>Say Hello</span>
-                Say Hello
-                </a>
+                <span href='https://www.facebook.com/'cclassName='link'>Say Hello</span>               
             </div>
       </div>
-    </footer>
   );
 };
 
--- src/frontend/homepage/homepage.js
+++ src/frontend/homepage/homepage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Footer from '../footer/footer';
 
 function HomePage() {
   return (
     <div>
       <h2>This is the Home Page</h2>
+      <Footer />
     </div>
   );
 }
