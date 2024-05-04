6f37675aed0389ae12ba1ac6cf7f3e0898f96067
Phuong Anh <119726597+PhuongAnh2212@users.noreply.github.com>
2024-05-04 21:52:17 +0700

    EOD commit

--- src/frontend/footer/footer.js
+++ src/frontend/footer/footer.js
@@ -4,23 +4,42 @@ import React from 'react';
 
 const Footer = () => {
   const footerStyle = {
-    // background: 'linear-gradient(to top, rgba(0, 15, 98, 1), rgba(4, 50, 141, 1), rgba(8, 84, 185, 1))',
     background: 'radial-gradient(ellipse at center, rgba(0, 15, 98, 1), rgba(4, 50, 141, 1)',
     color: '#fff',
     textAlign: 'center',
-    padding: '1rem',
+    padding: '10%',
     position: 'relative',
     bottom: 0,
-    width: '100%',
+    width: 'auto',
   };
 
   return (
     <footer style={footerStyle}>
-        <div className='grid-container'>
-            <div className='formlink'>
+      <div className='container'>
+        <div className='sayhello-container'>
                 {/* Use <a> tag for external link */}
                 <a href='https://www.facebook.com/'className='link'>Say Hello</a>               
-            </div>
+        </div>
+        <div className='grid-container'>
+          <div class="grid-item">
+            <h3>Vu An-Lam</h3>
+            <h3>Founder</h3>
+            <p>Email: founder1@example.com</p>
+            <p>Phone: 123-456-7890</p>
+          </div>
+          <div class="grid-item">
+            <h3>Hoa Leo</h3>
+            <h4>Lead of Communication Department</h4>
+            <p>Email: founder2@example.com</p>
+            <p>Phone: 123-456-7890</p>
+          </div>
+          <div class="grid-item">
+            <h3>Phuong Anh-Pham Doan</h3>
+            <h4>Tech Lead and Project Manager</h4>
+            <p>Email: founder3@example.com</p>
+            <p>Phone: 123-456-7890</p>
+          </div>
+        </div>
       </div>
     </footer>
   );
