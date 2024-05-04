0a0a90c76692f73e57afbb9551e60f93bd6abe88
Phuong Anh <119726597+PhuongAnh2212@users.noreply.github.com>
2024-05-04 19:52:03 +0700

    Revert "Merge pull request #5 from PhuongAnh2212/main"
    This reverts commit 4388b41009b174133d4bc8ab2ada5778f409a0af, reversing
    changes made to d3630181c284b18304f7cd05e91f2ce9a97d1206.

--- README.md
+++ README.md
@@ -1,13 +1,2 @@
 # dhlab-web
  
-### How to use
-
-    npm install 
-    npm start
-
-
-### People of this project
-Pham Doan Phuong Anh, UI/UX designer, frontend developer
-
-
-Khoa Tran, frontend developer
--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'; //stylesheet
-import Navbar from './frontend/navbar/navbar';
-import WhoAreWe from './frontend/whoarewe/whoarewe';
-import Archives from './frontend/archives/archives';
-import HomePage from './frontend/homepage/homepage';
+import Footer from './UI/footer/footer';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Navbar />
+    <><div>
+      <Footer/>
+    </div><BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/whoarewe" element={<WhoAreWe />} />
-          <Route path="/archives" element={<Archives />} />
+          <Route path="/footer" element={<Footer />} />
         </Routes>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter></>
   );
 };
 
